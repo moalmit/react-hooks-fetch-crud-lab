07a3229c1bd98a67246b94c96d693dbde98f5275
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-06-04 00:13:47 -0700

    Complete assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+//step-7 include onAddQuestion prop
+//step-12 include onDelete prop
+function QuestionItem({ question, onAddQuestion, onDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +11,29 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+//step-8 handle add question click
+function handleAddQuestion (e){
+  onAddQuestion(id, parseInt(e.target.value))
+}
+
+//step-13 handle delete click
+function handleDelete(){
+  onDelete(id)
+}
+
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        {/* step-9 add onChange event */}
+        <select defaultValue={correctIndex} onChange={handleAddQuestion}>
+          {options}</select>
       </label>
-      <button>Delete Question</button>
+      {/* step-14 add onDelete evennt */}
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,72 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+//step-3 import QuestionItem comp
+import QuestionItem from "./QuestionItem"
 
+
+
+//step-4 pass jsondata state prop from App comp and add useState to import
 function QuestionList() {
+
+//step-1 add state
+const [questions, setQuestions] = useState([])
+//step-2 fetch GET
+useEffect(()=>
+fetch("http://localhost:4000/questions")
+.then(r=>r.json())
+.then(questions=>setQuestions(questions))
+, [])
+
+//step-3 display questions after GET fetch
+  const questionItems = questions.map((q) => (
+    <QuestionItem 
+      key={q.id} 
+      question={q}
+      // step=6 add onAddQuestion prop
+      onAddQuestion={handlePatch}
+      // step-11 add onDelete prop
+      onDelete={handleDelete} />
+  ))
+
+//step-5 fetch PATCH
+function handlePatch(id, correctIndex){
+  fetch(`http://localhost:4000/questions/${id}`,{
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({correctIndex}),
+  })
+  .then(r=>r.json())
+  .then(updatedQuestion=>{
+    const updatedQuestions = questions.map(q=>{
+      if (q.id === updatedQuestion.id) {return updatedQuestion}
+      else {return q}
+    })
+    setQuestions(updatedQuestions)
+  })
+}
+
+//step-10 fetch DELETE
+function handleDelete(id){
+  fetch(`http://localhost:4000/questions/${id}`,{
+    method: "DELETE",
+  })
+  .then(r=>r.json())
+  .then(()=>{
+    const updatedQuestions = questions.filter(q=>q.id !== id)
+    setQuestions(updatedQuestions)
+  })
+}
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {/* display QuestionItem components here after fetching */}
+        {/* step-4 display after fetch GET */}
+        {questionItems}
+      </ul>
     </section>
   );
 }
